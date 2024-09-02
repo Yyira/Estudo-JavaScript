317a874ec3311857405b65ff7e775559ad8dfaf1
icaro <irenanlima16@gmail.com>
2024-09-02 17:02:13 -0300

    varias atividades

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+let vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+console.log("Dirija sempre usando cinto de segurança!")
+
+if (vel >=60){ //condição simples
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!")
+}
